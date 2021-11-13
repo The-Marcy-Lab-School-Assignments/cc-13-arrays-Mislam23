82278e72f6638cb817b065346ddcbfb5f31de41a
Mohammad Islam <islam.mohammad.student@gmail.com>
2021-11-13 13:35:03 +0000

    forgot to commit i think

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //write your code here
+function averageInArray(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i]
+  }
+  return sum / array.length
+}
+
+console.log(averageInArray([2,2,2,2,2]))
