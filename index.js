326ad94a2f43678fca8d6ff5067a1a5f02fa1fe2
Mohammad Islam <91222026+Mislam23@users.noreply.github.com>
2021-11-13 07:47:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //write your code here
+array = [1, 2, 3, 2, 2];
+
+function averageInArray (array) {
+	sum  = 0
+  for (let i = 0; i < array.length; i++) {
+  sum += array[i]
+  }
+  return sum/array.length
+}
+
+
+console.log(averageInArray(array))
